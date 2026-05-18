e99de0d97dfec13b11c6b49423899f8da457c4ba
Rishabh Tiwari <rishabhtiwari3538@gmail.com>
2026-05-18 14:50:52 +0530

    feat: update user schema with organisation and tour status fields and implement new login view

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
